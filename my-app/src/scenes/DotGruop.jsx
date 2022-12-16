5d71e8dc678a8649a2f5522a22ae62ce4f39ab19
Tarek994 <ghali.tarek.123@hotmail.com>
2022-12-16 23:40:29 +0200

    new(DotGroup): configure the DotGroup component

---
+++ my-app/src/scenes/DotGruop.jsx
@@ -0,0 +1,10 @@
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+
+const DotGruop = ({selectedPage, setSelectedPage}) => {
+  return (
+    <div>DotGruop</div>
+  )
+}
+
+export default DotGruop;
