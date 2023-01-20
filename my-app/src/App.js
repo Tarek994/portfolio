ae1448c4245917145a0168309315dda8fe8496ac
Tarek994 <ghali.tarek.123@hotmail.com>
2023-01-20 13:04:23 +0200

    style(LineGradient): set the LineGradient position in the App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,8 @@ import DotGroup from "./scenes/DotGroup";
 import Landing from "./scenes/Landing";
 import useMediaQuery from "./hooks/useMediaQuery";
 import Navbar from "./scenes/Navbar";
+import LineGradient from "./components/LineGradient";
+
 
 
 function App() {
@@ -35,6 +37,7 @@ function App() {
         )}
         <Landing setSelectedPage={setSelectedPage}/>
       </div>
+      <LineGradient/>
     </div>
   );
 }
