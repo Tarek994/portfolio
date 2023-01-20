ea63429f2e3bb61b85a7c706ff75165937695ac9
Tarek994 <ghali.tarek.123@hotmail.com>
2023-01-20 18:12:44 +0200

    style(contact): set the design of the name of the contact section

--- my-app/src/scenes/Contact.jsx
+++ my-app/src/scenes/Contact.jsx
@@ -3,8 +3,44 @@ import { useForm } from "react-hook-form";
 import { motion } from "framer-motion";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
+    const {
+      register,
+      trigger,
+      formState: {errors}
+    } = useForm()
+
+    const onSubmit = async (e) => {
+      const isValid = await trigger();
+      if (!isValid){
+        e.preventDefault();
+      }
+    }
+
+    return (
+    <section id="contact" className="py-48">
+      {/* HEADINGS */}
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, x: 50 },
+          visible: { opacity: 1, x: 0 },
+        }}
+        className="flex justify-end w-full"
+      >
+        <div>
+          <p className="font-playfair font-semibold text-4xl">
+            <span className="text-yellow">CONTACT ME</span> TO GET STARTED
+          </p>
+          <div className="flex md:justify-end my-5">
+            <LineGradient width="w-1/2" />
+          </div>
+        </div>
+      </motion.div>
+
+    </section>
   )
 }
 
