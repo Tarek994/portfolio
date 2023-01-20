93555ced379882d18a48e175c53bfaafe086f8ba
Tarek994 <ghali.tarek.123@hotmail.com>
2023-01-20 17:22:17 +0200

    style(testimonials): create the first block of the component

---
+++ my-app/src/scenes/Testimonials.jsx
@@ -0,0 +1,13 @@
+import LineGradient from "../components/LineGradient";
+import {motion} from "framer-motion";
+
+
+const Testimonials = () => {
+  return (
+    <section id="testimonials" className="pt-32 pb-16">
+        
+    </section>
+  )
+}
+
+export default Testimonials
