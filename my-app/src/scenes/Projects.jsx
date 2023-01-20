63066d6da889e8876b685d76af7138a3a3a1356f
Tarek994 <ghali.tarek.123@hotmail.com>
2023-01-20 15:45:00 +0200

    style(Projects): set up the style of the projects position

--- my-app/src/scenes/Projects.jsx
+++ my-app/src/scenes/Projects.jsx
@@ -14,11 +14,31 @@ const container = {
     hidden: { opacity: 0, scale: 0.8 },
     visible: { opacity: 1, scale: 1 },
   };
+
+  const Project = ({ title }) => {
+    const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
+      bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
+    const projectTitle = title.split(" ").join("-").toLowerCase();
+  
+    return (
+      <motion.div variants={projectVariant} className="relative">
+        <div className={overlayStyles}>
+          <p className="text-2xl font-playfair">{title}</p>
+          <p className="mt-7">
+            Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla
+            porttitor accumsan tincidunt.
+          </p>
+        </div>
+        <img src={`../assets/${projectTitle}.jpeg`} alt={projectTitle} />
+      </motion.div>
+    );
+  };
   
 
 const Projects = () => {
   return (
     <section id="projects" className="pt-48 pd-48">
+        {/* HEADINGS */}
         <motion.div
         className="md:w-2/5 mx-auto text-center"
         initial="hidden"
@@ -45,6 +65,7 @@ const Projects = () => {
         </p>
       </motion.div>
 
+        {/* PROJECTS */}
       <div className="flex justify-center">
         <motion.div
           className="sm:grid sm:grid-cols-3"
@@ -53,6 +74,15 @@ const Projects = () => {
           whileInView="visible"
           viewport={{ once: true, amount: 0.2 }}
         >
+            {/* ROW 1 */}
+          <div
+            className="flex justify-center text-center items-center p-10 bg-red
+              max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
+          >
+            BEAUTIFUL USER INTERFACES
+          </div>
+          <Project title="Project 1" />
+          <Project title="Project 2" />
         </motion.div>
         </div>
     </section>
