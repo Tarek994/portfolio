a35b5f365f8cdc5e681a97c9da996ba330da65f1
Tarek994 <ghali.tarek.123@hotmail.com>
2023-01-20 17:47:11 +0200

    style(contact): set up the contact section

---
+++ my-app/src/scenes/Contact.jsx
@@ -0,0 +1,11 @@
+import LineGradient from "../components/LineGradient";
+import { useForm } from "react-hook-form";
+import { motion } from "framer-motion";
+
+const Contact = () => {
+  return (
+    <div>Contact</div>
+  )
+}
+
+export default Contact
