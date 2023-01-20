031899f141f7f0e86b3183ab80b152e3ee05f25f
Tarek994 <ghali.tarek.123@hotmail.com>
2023-01-20 17:34:35 +0200

    style(testimonials): set the cards of the testimonial page

--- my-app/src/scenes/Testimonials.jsx
+++ my-app/src/scenes/Testimonials.jsx
@@ -26,6 +26,67 @@ const Testimonials = () => {
           ut fames odio in at. At magna ornare dictum lectus.
         </p>
       </motion.div>
+
+        {/* TESTIMONIALS */}
+      <div className="md:flex md:justify-between gap-8">
+        <motion.div
+          className="mx-auto relative bg-blue max-w-[400px] h-[350px] flex flex-col justify-end p-16 mt-48
+            before:absolute before:top-[-120px] before:-ml-[110px] before:left-1/2 before:content-person1"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.6 }}
+            variants={{
+            hidden: { opacity: 0, scale: 0.8 },
+            visible: { opacity: 1, scale: 1 },
+          }}
+        >
+          <p className="font-playfair text-6xl">“</p>
+          <p className="text-center text-xl">
+            A auctor pharetra hendrerit mattis amet etiam interdum platea.
+          </p>
+        </motion.div>
+        
+        <motion.div
+          className="mx-auto relative bg-red max-w-[400px] h-[350px] flex flex-col justify-end p-16 mt-48
+            before:absolute before:top-[-120px] before:-ml-[110px] before:left-1/2 before:content-person2"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.6 }}
+            variants={{
+            hidden: { opacity: 0, scale: 0.8 },
+            visible: { opacity: 1, scale: 1 },
+          }}
+        >
+          <p className="font-playfair text-6xl">“</p>
+          <p className="text-center text-xl">
+            Aliquam aliquet integer ut fames odio in at. At magna ornare dictum
+            lectus.
+          </p>
+        </motion.div>
+
+        <motion.div
+          className="mx-auto relative bg-yellow max-w-[400px] h-[350px] flex flex-col justify-end p-16 mt-48
+            before:absolute before:top-[-120px] before:-ml-[110px] before:left-1/2 before:content-person3"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.4, duration: 0.6 }}
+            variants={{
+            hidden: { opacity: 0, scale: 0.8 },
+            visible: { opacity: 1, scale: 1 },
+          }}
+        >
+          <p className="font-playfair text-6xl">“</p>
+          <p className="text-center text-xl">
+            Fames odio in at. At magna ornare dictum lectus.
+          </p>
+        </motion.div>
+
+
+      </div>
+
     </section>
   )
 }
