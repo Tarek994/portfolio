f0c44b776e7492a3cdfb9fadecb617c4664056c2
Ali Ghali <ali.ghali@zalando.de>
2023-01-20 19:06:43 +0100

    fix(components): use correct project image

--- my-app/src/scenes/Projects.jsx
+++ my-app/src/scenes/Projects.jsx
@@ -1,45 +1,47 @@
 import LineGradient from "../components/LineGradient";
 import { motion } from "framer-motion";
-import project_1 from "../assets/project-1.jpeg"
+import { projectImages } from "../assets/images";
+
+
 const container = {
-    hidden: {},
-    visible: {
-      transition: {
-        staggerChildren: 0.2,
-      },
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.2,
     },
-  };
+  },
+};
 
-  const projectVariant = {
-    hidden: { opacity: 0, scale: 0.8 },
-    visible: { opacity: 1, scale: 1 },
-  };
+const projectVariant = {
+  hidden: { opacity: 0, scale: 0.8 },
+  visible: { opacity: 1, scale: 1 },
+};
 
-  const Project = ({ title }) => {
-    const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
+const Project = ({ title }) => {
+  const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
       bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
-    const projectTitle = title.split(" ").join("-").toLowerCase();
-  
-    return (
-      <motion.div variants={projectVariant} className="relative">
-        <div className={overlayStyles}>
-          <p className="text-2xl font-playfair">{title}</p>
-          <p className="mt-7">
-            Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla
-            porttitor accumsan tincidunt.
-          </p>
-        </div>
-        <img src={`../assets/${projectTitle}.jpeg`} alt={projectTitle} />
-      </motion.div>
-    );
-  };
-  
+  const projectTitle = title.split(" ").join("-").toLowerCase();
+
+  return (
+    <motion.div variants={projectVariant} className="relative">
+      <div className={overlayStyles}>
+        <p className="text-2xl font-playfair">{title}</p>
+        <p className="mt-7">
+          Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla
+          porttitor accumsan tincidunt.
+        </p>
+      </div>
+      <img src={projectImages[projectTitle]} alt={projectTitle} />
+    </motion.div>
+  );
+};
+
 
 const Projects = () => {
   return (
     <section id="projects" className="pt-48 pd-48">
-        {/* HEADINGS */}
-        <motion.div
+      {/* HEADINGS */}
+      <motion.div
         className="md:w-2/5 mx-auto text-center"
         initial="hidden"
         whileInView="visible"
@@ -65,7 +67,7 @@ const Projects = () => {
         </p>
       </motion.div>
 
-        {/* PROJECTS */}
+      {/* PROJECTS */}
       <div className="flex justify-center">
         <motion.div
           className="sm:grid sm:grid-cols-3"
@@ -74,7 +76,7 @@ const Projects = () => {
           whileInView="visible"
           viewport={{ once: true, amount: 0.2 }}
         >
-            {/* ROW 1 */}
+          {/* ROW 1 */}
           <div
             className="flex justify-center text-center items-center p-10 bg-red
               max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
