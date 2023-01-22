da1275b21c37535eba5c5c3c9c41355b59bcabb9
Tarek994 <ghali.tarek.123@hotmail.com>
2023-01-22 15:51:08 +0200

    style(assets-images): add all projects image to the projects

--- my-app/src/assets/images.jsx
+++ my-app/src/assets/images.jsx
@@ -2,10 +2,16 @@ import project_1 from "./project-1.jpeg"
 import project_2 from "./project-2.jpeg"
 import project_3 from "./project-3.jpeg"
 import project_4 from "./project-4.jpeg"
+import project_5 from "./project-5.jpeg"
+import project_6 from "./project-6.jpeg"
+import project_7 from "./project-7.jpeg"
 
 export const projectImages = {
     "project-1": project_1,
     "project-2": project_2,
     "project-3": project_3,
     "project-4": project_4,
+    "project-5": project_5,
+    "project-6": project_6,
+    "project-7": project_7,
 }
